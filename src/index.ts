e7c7c1e3964ddde01fa8d4107824d7fb495e46f8
drizzer14 <drizzer14@gmail.com>
2020-04-06 13:39:10 +0300

    Change index.ts import order

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
+export { default as Form } from './components/Form';
 export { default as Field } from './components/Field';
 export { default as FieldSet } from './components/FieldSet';
-export { default as Form } from './components/Form';
