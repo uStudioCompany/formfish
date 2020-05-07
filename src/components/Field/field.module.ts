de7d4824d43b8beb8c7ffa2c4b957ec17fed8d1a
drizzer14 <drizzer14@gmail.com>
2020-05-07 09:57:22 +0300

    fix(Field): change props comparison method

--- src/components/Field/field.module.ts
+++ src/components/Field/field.module.ts
@@ -1,8 +1,8 @@
 import type { FieldProps } from './field.types';
 
-const replacer = (key: string, value: any): any => {
-  if (key === 'children') {
-    return (value as { props: any }).props;
+const replacer = (key: string, value: { props: any }): any => {
+  if (typeof value === 'object' && 'props' in value) {
+    return value.props;
   }
 
   return value;
