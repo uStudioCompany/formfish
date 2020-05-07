3e4eab9685a7cc966070a4073842b37bf26c7f84
drizzer14 <drizzer14@gmail.com>
2020-05-07 09:50:51 +0300

    fix(Field): compare children's props too

--- src/components/Field/field.module.ts
+++ src/components/Field/field.module.ts
@@ -2,7 +2,7 @@ import type { FieldProps } from './field.types';
 
 const replacer = (key: string, value: any): any => {
   if (key === 'children') {
-    return undefined;
+    return (value as { props: any }).props;
   }
 
   return value;
--- src/test/App.tsx
+++ src/test/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, Dispatch, useEffect, useReducer, useState } from 'react';
 import Checkbox from 'ustudio-ui/components/Checkbox';
 import Text from 'ustudio-ui/components/Text';
 import Field from '../components/Field';
@@ -9,19 +9,47 @@ import { Input } from './Input';
 const encode = (value = '') => value;
 const decode = (value = '') => value;
 
-const Ctx = createContext({} as { value: boolean; setValue: (value: boolean) => void });
+const Ctx = createContext(
+  {} as { state: { value: boolean }; setValue: Dispatch<{ type: 'set_value'; payload: boolean }> }
+);
+
+const reducer = (state: { value: boolean }, action: { type: 'set_value'; payload: boolean }) => {
+  return { ...state, value: action.payload };
+};
 
 const CtxProvider: React.FC = ({ children }) => {
-  const [value, setValue] = useState(false);
+  const [state, setValue] = useReducer(reducer, { value: false });
 
-  return <Ctx.Provider value={{ value, setValue }}>{children}</Ctx.Provider>;
+  return <Ctx.Provider value={{ state, setValue }}>{children}</Ctx.Provider>;
+};
+
+const Req: React.FC<{
+  state: { value: boolean };
+  dispatch: Dispatch<{ type: 'set_value'; payload: boolean }>;
+  isActive: boolean;
+}> = ({ state, dispatch, isActive }) => {
+  const [value, setValue] = useState(state.value);
+
+  useEffect(() => {
+    dispatch({ type: 'set_value', payload: value });
+  }, [value]);
+
+  return (
+    <>
+      <Field name="Text" watch={setValue}>
+        <Checkbox aria-label={`${value}`} isDisabled={!isActive} />
+      </Field>
+
+      <Text color={state.value ? 'pink' : 'green'}>{`${state.value}`}</Text>
+    </>
+  );
 };
 
 const App: React.FC = () => {
   const handleSubmit = (formState: unknown) => console.log(formState);
   const watch = (value: unknown) => console.log(value);
 
-  const [value, setValue] = useState(false);
+  const [isActive, setActive] = useState(false);
 
   return (
     <CtxProvider>
@@ -29,14 +57,12 @@ const App: React.FC = () => {
         <FieldSet name="topmost">
           <FieldSet name="top">
             <FieldSet name="fieldset">
-              <Field name="Text" watch={setValue}>
-                <Checkbox />
-              </Field>
-
-              <Text color={value ? 'pink' : 'green'}>{`${value}`}</Text>
+              <Ctx.Consumer>{({ state, setValue }) => <Req state={state} dispatch={setValue} isActive={isActive} />}</Ctx.Consumer>
             </FieldSet>
           </FieldSet>
         </FieldSet>
+
+        <Checkbox value={isActive} onChange={setActive} />
 
         <button type="submit">Submit</button>
       </Form>
